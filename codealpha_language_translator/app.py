3b3aa6f427b3617fe3f8351a02ff136dfcf6f9a2
Renu881 <renukadevisubramanian526@gmail.com>
2026-01-08 08:58:08 +0530

    Fix language translator folder (remove sub-repo)

---
+++ codealpha_language_translator/app.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template, request
+from googletrans import Translator
+
+app = Flask(__name__)
+translator = Translator()
+
+@app.route("/", methods=["GET", "POST"])
+def index():
+    translated_text = ""
+    if request.method == "POST":
+        text = request.form["text"]
+        src = request.form["source"]
+        dest = request.form["target"]
+
+        result = translator.translate(text, src=src, dest=dest)
+        translated_text = result.text
+
+    return render_template("index.html", translated_text=translated_text)
+
+if __name__ == "__main__":
+    app.run(debug=True)
